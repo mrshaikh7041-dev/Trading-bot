8fc47513154298ed473370a6d69c2b04b859faa8
mrshaikh7041-dev <mrshaikh7041@gmail.com>
2025-11-21 18:08:29 +0530

    Change LOT_SIZE from 0.05 to 0.02

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from datetime import datetime, timezone, timedelta
 SYMBOL = "BNB/USDT"
 TIMEFRAME = "1m"
 
-LOT_SIZE = 0.05          # qty send in market order
+LOT_SIZE = 0.02          # qty send in market order
 RSI_PERIOD = 14
 RSI_LOW = 10
 RSI_HIGH = 37
